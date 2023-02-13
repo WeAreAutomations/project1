7322fee1c66f796568cb41e2507efa6d4545ba6e
yanuvelir <yanuvelir@gmail.com>
2023-02-13 17:57:09 +0200

    Initial commit

---
+++ src/Java1.java
@@ -0,0 +1,20 @@
+public class Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Automation " + " Automation");
+        double age, legs, tail;
+        String name = "Bob";
+        age = 12;
+        legs = 7;
+        tail = 1;
+        System.out.println(name);
+        System.out.println(name + " "  + age + legs + tail);
+        //int sum = age + legs + tail;
+        //System.out.println(sum);
+        double dobl = 4.2;
+        double div = age/legs;
+        System.out.println(div + " Деление");
+
+
+    }
+}
